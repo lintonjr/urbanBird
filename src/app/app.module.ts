13cb2e1782a0409752ff12959c4603772c47892f
Linton Junior <lintonjr@hotmail.com>
2020-03-17 16:23:52 -0400

    layout changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
